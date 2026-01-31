9a1262384cbf7ff4fe1f60d77dbfa64ac9b04e65
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-01-31 14:21:39 +0600

    added for-of-loop.js file

---
+++ for-of-loop.js
@@ -0,0 +1,21 @@
+const numbers = [12, 30, 45, 33, 46, 22];
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+}
+
+
+const flowers = ['rose', 'sunflower', 'marigold', 'lily'];
+console.log(flowers);
+
+for(const flower of flowers){
+    console.log(flower);
+}
+
+let prices = [200, 500, 450];
+console.log(prices);
+ for(let price of prices){
+    console.log(price);
+    console.log('Price is so heigh');
+ }
