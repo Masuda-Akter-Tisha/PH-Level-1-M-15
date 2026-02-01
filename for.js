c9e5e832557bda34974c5341c512f0e8ea9db6e2
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-01 19:56:19 +0600

    added more files

---
+++ for.js
@@ -0,0 +1,11 @@
+//normal while loop
+// let num = 1;
+// while(num < 61){
+//     console.log('I Love You Abbo' , num);
+//     num ++;
+// }
+
+//normal for loop
+for (let num = 1; num < 61; num ++){
+    console.log('I LOve You Abbo', num);
+}
---
+++ odd-even.js
@@ -0,0 +1,21 @@
+// for (let i = 1; i <= 10; i ++){
+//     console.log(i);
+//     if(i % 2 == 0){
+//         console.log('even number:', i);
+//     }
+// }
+
+// for (let i = 2; i <= 10; i += 2){
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i ++){
+//     console.log(i);
+//     if(i % 2 !== 0){
+//         console.log('odd number:', i);
+//     }
+// }
+
+for (let i = 1; i <= 10; i += 2){
+    console.log(i);
+}
