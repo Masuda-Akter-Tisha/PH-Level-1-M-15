797b995f221679d52eb1dc55e3c21538f7d0076f
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-01 12:11:36 +0600

    added while.js file

---
+++ while.js
@@ -0,0 +1,13 @@
+const number = 10;
+if(number > 7){
+    console.log('something is cooking new');
+}
+console.log('something is not cooking new');
+
+let num = 0;
+while(num <= 5){
+    console.log('looping',num);
+    //num = num + 1;
+    //num += 1;
+    num ++;
+}
