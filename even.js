5908f540a0791880d271efca6feb72ca960802a3
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-01 19:10:26 +0600

    added more files

---
+++ even.js
@@ -0,0 +1,25 @@
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+
+//     if(num % 2 === 0){
+//         console.log('Even Number:', num);
+//     }
+
+//     num ++;
+// }
+
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+
+//     if(num % 2 !== 0){
+//         console.log('odd number:', num);
+//     }
+//     num ++;
+// }
+let num = 1;
+while(num <= 10){
+    console.log(num);
+    num += 2;
+}
---
+++ sum.js
@@ -0,0 +1,18 @@
+// let num = 1;
+// let sum = 0;
+// while(num <= 10){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log('sum:', sum);
+//     num ++;
+
+// }
+
+let num = 1;
+let sub = 0;
+while(num <= 10){
+    console.log(num);
+    sub = num - sub;
+    console.log(sub);
+    num ++;
+}
