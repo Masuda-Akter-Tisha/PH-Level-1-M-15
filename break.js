bb7f191827df30c044a80f2561ce74ce2935ff14
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-02 10:59:29 +0600

    added break.js file

---
+++ break.js
@@ -0,0 +1,17 @@
+// for (let i = 1; i <= 20; i ++){
+//      console.log(i);
+//     if(i >= 15){
+//         break;
+//     }
+// }
+// console.log('The loop exited because of break')
+
+let n = 78;
+while(n > 60){
+     console.log(n);
+    if(n < 64){
+        break;
+    }
+    //  console.log(n);
+    n --;
+}
